e48ecb60f4bba46249ddb2c14c1d322a11014e6f
ravisiswaliya <ravisiswaliya@gmail.com>
2017-09-13 20:09:32 +0530

    add post feature in homepage

--- account/models.py
+++ account/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager,self).get_queryset().filter(city='Hisar')
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -13,6 +16,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    Hisar = UserProfileManager()
 
 def create_profile(sender, **kwargs):
     if kwargs['created']:
